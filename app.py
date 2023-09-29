698d39532bf7c6cd62e38c7bbaeafb55a8619a47
dnetcloud <solubiyi845@stu.ui.edu.ng>
2023-09-29 20:52:12 +0100

    update code

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, Simple Flak Apllication"
+def hello_geek():
+    return '<div style=\"text-align: center;\"><h1>Hello from DnetCloud!</h1><p>Welcome to GCP DevOps Project.</p><p></p></div>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
